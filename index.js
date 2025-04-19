eb6a51884263fa98e05a09de80d88729b3ee554a
Srihas Reddy Challa <challasrihasreddy@gmail.com>
2025-04-20 01:02:02 +0530

    add routes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import authRouter from './routes/auth.routes.js'
+import userRouter from './routes/user.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
