5e3ab500f229a87d668b80881aebdc95c80a7870
Srihas Reddy Challa <challasrihasreddy@gmail.com>
2025-05-15 20:55:25 +0530

    set env variables

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express'
+import { PORT } from './config/env.js'
 import authRouter from './routes/auth.routes.js'
 import userRouter from './routes/user.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
 
 const app = express()
-const port = 3000
 
 app.use(express.json())
 app.use("/api/v1/auth", authRouter)
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
     res.send("Hello World!")
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
