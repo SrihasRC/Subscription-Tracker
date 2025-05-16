573439c689092d677fc115d1b3329d512e74f77c
Srihas Reddy Challa <challasrihasreddy@gmail.com>
2025-05-16 22:29:18 +0530

    create error handler middleware

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-import express from 'express'
+import express, { urlencoded } from 'express'
+import cookieParser from 'cookie-parser'
 import { PORT } from './config/env.js'
 import authRouter from './routes/auth.routes.js'
 import userRouter from './routes/user.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
 import connectToDatabase from './database/mongodb.js'
+import errorMiddleware from './middlewares/error.middleware.js'
 
 const app = express()
 
 app.use(express.json())
+app.use(errorMiddleware)
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
