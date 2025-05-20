b8ba84bfb3a97f90f3a4c1959724bdf5f13f5029
Srihas Reddy Challa <challasrihasreddy@gmail.com>
2025-05-20 15:13:00 +0530

    send emails

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -3,6 +3,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const { serve } = require("@upstash/workflow/express");
 import Subscription from '../models/subscription.model.js';
+import { sendReminderEmail } from '../utils/send-email.js'
 
 const REMINDERS = [7, 5, 2, 1]
 
@@ -46,6 +47,11 @@ const sleepUntilReminder = async (context, label, date) => {
 const triggerReminder = async (context, label, subscription) => {
   return await context.run(label, async () => {
     console.log(`Triggering ${label} reminder`);
-    
+
+    await sendReminderEmail({
+      to: subscription.user.email,
+      type: label,
+      subscription,
+    })
   })
 }
